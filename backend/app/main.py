79b26a625e64addb855eb00a4df57808c3d018f1
rajendra912 <muhrajendra09@gmail.com>
2026-05-02 23:23:03 +0700

    fix: 2nd app/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from app.routers import transactions
-from app.routers import voice_input          # impor dulu
+from app.routers import voice_input
 
-app = FastAPI(title="VoiceBank API", version="1.0.0")  # ← ini harus di atas semua include_router
+app = FastAPI(title="VoiceBank API", version="1.0.0")
 
 app.include_router(transactions.router, prefix="/api", tags=["Transactions"])
 app.include_router(voice_input.router, prefix="/api", tags=["Voice Input"])
